9f21b707fa0d99eed361d1198e4541b03ce00302
Billsa <samuraibilson@gmail.com>
2022-09-08 12:05:41 -0700

    size is not granular

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,15 @@
 
 /**
  * Main - prints the size of the various types in the computer,
- * it is compiled on 
- * Return : 0 if the program ends successfully 
+ * it is compiled on
+ * Return : 0 if the program ends successfully
  */
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n" sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	
 	return (0);
