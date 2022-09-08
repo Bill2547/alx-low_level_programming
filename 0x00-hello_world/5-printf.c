af84329deebf199c304e6a2e5dfee0670f273d29
Billsa <samuraibilson@gmail.com>
2022-09-08 11:29:19 -0700

    hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  *main - prints the string indicated
  *
- *return- afterthe successful run no status is 0
+ *return: 0 afterthe successful run no status is
  *
  */
 
 int main(void)
 {
-	printf ("with proper gramer,but the outcome is a piece of art,");
+	printf ("with proper gramer,but the outcome is a piece of art,\n");
 	return (0);
 }
