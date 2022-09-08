f6ec89fe251347149fcb65dbcf97504c6fb06872
Billsa <samuraibilson@gmail.com>
2022-09-08 11:09:49 -0700

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- *  this prints the words in the string
+ * main- this prints the words in the string
+ * Return: 0 if the stting is at the end 
 */
-int main()
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 
 }
