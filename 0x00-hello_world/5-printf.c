5cb4ecad8914d6f566081ced144b882fffa59023
Billsa <samuraibilson@gmail.com>
2022-09-08 11:19:32 -0700

    hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the string indicated
+ *
+ * return- afterthe successful run no status is 0
+ *
+ */
+
+int main (void)
+{
+	printf("\"with proper gramer,but the outcome is a piece of art,");
+	return(0);
+}
