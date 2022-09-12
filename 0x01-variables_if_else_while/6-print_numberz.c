02988e7bc360f01eeeb4ef1fb05ba135ae3967e4
Billsa <samuraibilson@gmail.com>
2022-09-12 11:30:34 -0700

    6 Numbuz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digits number of base 10
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{	
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
