d562c206018c0142144c46010b28993b02cb5e68
Billsa <samuraibilson@gmail.com>
2022-09-12 09:17:19 -0700

    Positive anything is better

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+/** 
+ * main- assign a number to the variable n each time it is executed print whether the number stored is positve or negative 
+ * return: 0 if program executed successfully 
+ */
+
+int main(void) 
+{
+	int n;
+		
+		if (n>0)
+			printf("%d is positive\n',n);
+		else if (n==0)
+			printf("%d is zero\n",n);
+		else if (n<0)
+			printf("%d is negative\",n);
+		return (0);
+}
