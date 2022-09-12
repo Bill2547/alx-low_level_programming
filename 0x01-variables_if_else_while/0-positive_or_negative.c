f4ff63ae451b62e33796386403781286cf5220c7
Billsa <samuraibilson@gmail.com>
2022-09-12 09:25:39 -0700

    Positive anything is better

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int n;
 		
-		if (n>0)
+		if (n > 0)
 			printf("%d is positive\n",n);
-		else if (n==0)
+		else if (n == 0)
 			printf("%d is zero\n",n);
-		else if (n<0)
-			printf("%d is negative\",n);
+		else if (n < 0)
+			printf("%d is negative\n",n);
 		return (0);
 }
