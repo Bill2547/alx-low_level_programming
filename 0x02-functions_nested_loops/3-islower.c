b16eb25a2f0c88fabc250ab5efccf1a0a6e8d436
Billsa <samuraibilson@gmail.com>
2022-09-13 08:44:39 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*int _islower - checks function for lower case
+*returns: 1 if c is lowercase
+*Returns: 0 otherwise
+*/
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
