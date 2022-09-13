2ac17e1c1f0f96733e18655b38f5af2f2f0051bb
Billsa <samuraibilson@gmail.com>
2022-09-13 10:07:32 -0700

    8 -24_hours

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 /**
 *_abs - computes the absolute value of an integer
 *@n: integer to be checked
-*return: absolute value of the integer
+*return: absolute value of the integer n
 */
 
 int _abs(int n)
