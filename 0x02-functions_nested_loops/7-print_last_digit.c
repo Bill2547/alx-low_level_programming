29af489020ef72dacfb4b8ef704ca9770a0709fe
Billsa <samuraibilson@gmail.com>
2022-09-13 09:54:02 -0700

    7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - prints the last digit of an number
- *@ n -  number whose last digit is to be printed
+ *@n: number whose last digit is to be printed
  * Return: value of the last digit
  */
 
