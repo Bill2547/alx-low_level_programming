61f04af6a810a5e8fcd6f8339ccf105d5ba99678
Billsa <samuraibilson@gmail.com>
2022-09-13 08:54:22 -0700

    4-isaplpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*_isalpha -checks for alphabetic character
+*returns: 1 if c is a letter ,lowecase or uppercase
+*returns: 0 if false
+*/
+
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
+}
