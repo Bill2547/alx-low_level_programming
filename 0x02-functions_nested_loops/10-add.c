b84491a08e0e62f1bb37b8abaf76102f0f460eac
Billsa <samuraibilson@gmail.com>
2022-09-14 09:31:19 -0700

    10-add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,9 @@
 
 /**
  * add - adds the value of two integers
- * a: first integer
- * b: second integer
+ *@a: first integer
+ *@b: second integer
+ *Return: sum of the two integers
  */
 
 int add(int a, int b)
