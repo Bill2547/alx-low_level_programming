e88c931bdb13541cc4570a82105b0850d4c0f8db
Billsa <samuraibilson@gmail.com>
2022-09-14 10:03:25 -0700

    11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
 
 void print_to_98(int n)
 {
-	int i;
+	int i, j;
 
 	if (n <=98)
 	{
@@ -20,7 +20,15 @@ void print_to_98(int n)
 			else if (i == 98)
 				printf("%d\n", i);
 		}
-		i++;
 	}
+	else if  ( n>= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf ("%d\n", j);
+		}
 }
 
