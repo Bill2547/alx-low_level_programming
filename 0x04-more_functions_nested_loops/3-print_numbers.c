d9b369679314edf5bc762f125e7bd5b0a9a1bf56
Billsa <samuraibilson@gmail.com>
2022-09-15 10:03:13 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * Print_numbers - prints numbers from 0 to 9
+ * Return: 1if Cis uppercase and 0 otherwise
+ */
+
+vouid print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
