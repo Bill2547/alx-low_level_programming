91f634efa18a0b5227a35a41ac88ed8c205ba5db
Billsa <samuraibilson@gmail.com>
2022-09-15 13:48:23 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - checks that the number is zero if given zero
+ * @i: the number to be checked
+ * Return: Always 0 if 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0 ;
+	
+	positive_or_negative(i);
+
+	return (0);
+}
