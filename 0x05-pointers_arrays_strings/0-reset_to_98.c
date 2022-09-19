1816b38ed97d7d21c3a811a2792965dbacc2cca5
Billsa <samuraibilson@gmail.com>
2022-09-19 09:55:41 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,6 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter and updates the
  * value it points to 98
- * @n:the integer
  * @*n: the pointer to be updated
  * Return: 0 if sucessful
  */
