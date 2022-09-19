8b37ba664d0f86982e909ae052ab0e8089a7e48a
Billsa <samuraibilson@gmail.com>
2022-09-19 10:56:05 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,7 +5,6 @@ int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void puts(char *str);
 void print_rev(char *s);
 void rev_strings(char *s);
 void puts(char *str);
