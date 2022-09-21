22a32bb8c7cbe84bbb33136df6044f4b40f70b44
Billsa <samuraibilson@gmail.com>
2022-09-21 09:53:42 -0700

    1-stncat.c

--- 0x06-pointers_arrays_strings/1-stncat.c
+++ 0x06-pointers_arrays_strings/1-stncat.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
+ * _strncat - concatenates two strings
  * @dest: the string to append
  * @src: the string to be appended
  * n:the bytes size to be used
  * Return: dest
  */
 
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
