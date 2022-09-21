ec80155334af32b2c8a9e4b4a161a45d97e52b4b
Billsa <samuraibilson@gmail.com>
2022-09-21 09:43:58 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char *_strcat(char *dest, char *src)
+
+	return (0);
+}
+
