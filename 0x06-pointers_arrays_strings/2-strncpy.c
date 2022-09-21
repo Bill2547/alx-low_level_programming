f974903da5ab9e0f77210fa497a0f880961613d3
Billsa <samuraibilson@gmail.com>
2022-09-21 10:02:47 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination of the string
+ * @src: the string to be appended
+ * n:the bytes size to be used
+ * Return: pointer to the resulting string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	
+	}
+	
+	return (dest);
+}
