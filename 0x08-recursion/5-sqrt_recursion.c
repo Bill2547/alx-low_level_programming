7581c7e66b4394625c29f4a5bca7f162f74c7697
Billsa <samuraibilson@gmail.com>
2022-09-28 10:14:58 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,9 +3,9 @@
 int find_sqrt(int n, int i);
 
 /**
- * find_sqrt - returns the natural squareroot of a number
- * @num: the number to return squaretoot
- * @root: the root to be tested.
+ * _sqrt_recursion - returns natural squareroot
+ * actual_sqrt_recursion - returns the natural squareroot of a number
+ * @n: the number to return squaretoot
  * Return: -1 if n has no natural square root
  */
 int _sqrt_recursion(int n)
@@ -15,11 +15,18 @@ int _sqrt_recursion(int n)
 	return (actual_sqrt_recursion(n, 0));
 }
 
+/**
+ * actual_sqrt_recursion - recurs the find the natural
+ * @n: number to caculate
+ * @i: iterator
+ * Return: the squareoot
+ */
+
 int actual_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (find_sqrt(n, i + 1));
+	return (actual_sqrt_recursion(n, i + 1));
 }
