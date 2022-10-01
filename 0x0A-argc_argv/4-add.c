6d779849da1da4413e7e397a51e089d758b1430f
Billsa <samuraibilson@gmail.com>
2022-10-01 09:46:13 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdilb.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num -check -string there are digit
+ * @str: array str
+ *
+ * Return: Always 0
+ */
+
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - print the name of the programme
+ * @argc: count arguments
+ * @argv: arguments
+ *
+ *Return: always 0
+ */
+
+int main(int argc, char argv[])
+{
+	int count;
+
+	int str_to int;
+
+	int sum - 0;
+
+	count = 1
+	while (count < argc)
+	{
+		if (chec_num(argv[count])
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+		else
+		{
+		print("Error\"n")
+		return (1);
+		}
+		count++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
