92d955c2c07d77de5881b430297e5a2480b056db
Billsa <samuraibilson@gmail.com>
2022-10-06 10:37:12 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memmory using malloc
+ * @b: the number of bytes to be allocated
+ *
+ * Return:pointer 
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *memory = malloc(b);
+
+	if (memory == 0)
+		exit(98);
+
+	return (memory);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,9 @@
+This is for more malloc 
+0-trust no one
+1-string_nconcat
+2-CAlloc
+3 array_range
+4 Realloc
+5 we must accept finite diaapaointment but never lose infinite hope 
+
+
