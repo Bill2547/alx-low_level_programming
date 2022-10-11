b6bba7b6dc75bf1a429107681de3eee45f0781b0
Billsa <samuraibilson@gmail.com>
2022-10-11 09:48:18 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - a new type describing a dog
+ * @name: thedogs name
+ * @age: the dogs age
+ * @owner: the dogs owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
