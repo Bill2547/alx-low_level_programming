62f26a9f821deec8efb42b0b168a3943ec2a4464
Billsa <samuraibilson@gmail.com>
2022-10-11 11:41:51 -0700

    0x0d

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of the file program was compiled fromfollowedby new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
