2f6564fdbcb531870cf48f7a252473fa5809d51a
Billsa <samuraibilson@gmail.com>
2022-10-13 09:53:03 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "functio_pointers.h"
+
+/**
+ * int_index -search for integer in array
+ * @array: - arrays
+ * @size: size of array
+ * @cmp: fuction to compare values
+ * Return: -1 else 0
+ */
+
+int int_index(int *array, int size, int(*cmp)(int))
+{
+	int i;
+
+	if (!array || !cmp)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i] != 0)
+				return (i);
+	}
+	return (-1);
+}
